00961eed35767f5aead1fd85bed3fc5ac9a0bcdb
ShahzaibAli4321 <sa7529693@gmail.com>
2025-09-03 20:46:21 +0500

    Adding animations of walking and idle

--- Assets/Scripts/Boy.cs
+++ Assets/Scripts/Boy.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class Boy : MonoBehaviour
 {
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow)
+        if(Input.GetKey(KeyCode.UpArrow))
         {
+            if (animator.GetBool("Forward") == false)
+            {
+                Debug.Log("Walking");
+            }
+            animator.SetBool("Forward", true);
+        }
 
+        if (!Input.GetKey(KeyCode.UpArrow))
+        {
+            if (animator.GetBool("Forward") == true)
+            {
+                Debug.Log("Not Walking");
+            }
+            animator.SetBool("Forward", false);
         }
     }
 }
